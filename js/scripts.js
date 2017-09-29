8b41966491201a6d2b25e03927004833f2420fc4
mgrobeelny <michalmaciejg@gmail.com>
2017-09-29 22:48:28 +0200

    init

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// scripts.js
+
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    //var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    
+    function getQuote() {
+        $.getJSON(quoteUrl, createTweet);
+    }
+    
+    function createTweet(input) {
+        if (!input.quoteAuthor.length) {
+            input.quoteAuthor = 'Unknown author';
+        }
+        
+        var tweetText = 'Quote of the day - ' + input.quoteText + 'Author: ' + input.quoteAuthor;
+        
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            $('.quote').text(input.quoteText);
+            $('.author').text('Author: ' + input.quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+    }
+
+$(document).ready(function() {   
+    getQuote();
+    
+    $('.trigger').click(function() {
+        getQuote();
+    });
+    
+});
+
+
